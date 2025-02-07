72046bccfd7b42a6140099dbe1cf384f9db3d132
01vaibhavnaik <148302295+01vaibhavnaik@users.noreply.github.com>
2025-02-07 14:41:50 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,71 @@
+<div class="newsletter">
+    <div class="content">
+        <span>get latest Sprinkle City updates</span>
+        <h1>subcribe our newsletter</h1>
+        <p>edrftgyhujikoljiuhgytfrdfghj</p>
+        <div class="input-field">
+            <input type="email" name="" placeholder="Enter your E-Mail">
+            <button class="btn">subscribe</button>
+        </div>
+        <p>No ads, No trails</p>
+        <div class="box-container">
+            <div class="box">
+                <div class="box-counter"><p class="counter">5000</p><i class="bx bx-plus"></i></div>
+                <h3>Successfully Trained</h3>
+                <p>Learners & counting</p>
+            </div>
+            <div class="box">
+                <div class="box-counter"><p class="counter">10000</p><i class="bx bx-plus"></i></div>
+                <h3>Certification seller</h3>
+                <p>Online sellers</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer>
+    <div class="content">
+        <div class="box">
+            <img src="image/logo.png">
+            <p>we are always in search of talented peaple. Don't be shy introduce yourself!</p>
+            <a href="contact.php"class="btn">contact now</a>
+        </div>
+        <div class="box">
+            <h3>my account</h3>
+            <a href=""><i class="bx bx-chevron-right"></i>My account</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Order history</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Wishlist</a>
+            <a href=""><i class="bx bx-chevron-right"></i>newsletter</a>           
+        </div>
+        <div class="box">
+            <h3>information</h3>
+            <a href=""><i class="bx bx-chevron-right"></i>about us</a>
+            <a href=""><i class="bx bx-chevron-right"></i>delivery information</a>
+            <a href=""><i class="bx bx-chevron-right"></i>privacy policy</a>
+            <a href=""><i class="bx bx-chevron-right"></i>terms & condition</a>           
+        </div>
+        <div class="box">
+            <h3>extras</h3>
+            <a href=""><i class="bx bx-chevron-right"></i>brands</a>
+            <a href=""><i class="bx bx-chevron-right"></i>gift Certificationn</a>
+            <a href=""><i class="bx bx-chevron-right"></i>affiliate</a>
+            <a href=""><i class="bx bx-chevron-right"></i>specials</a>           
+        </div>
+        <div class="box">
+            <h3>contact us</h3>
+            <p><i class="bx bxs-phone"></i>(+91) 878-5264-678 </p>
+            <p><i class="bx bxs-envelope"></i>icecream@gmail.com</p>
+            <p><i class="bx bxs-location-plus"></i>India</p>
+            <div class="icons">
+            <i class='bx bxl-facebook' ></i>
+            <i class='bx bxl-twitter' ></i>
+		    <i class='bx bxl-instagram-alt' ></i>
+		    <i class='bx bxl-linkedin' ></i>
+            </div>        
+        </div>
+    </div>
+    <div class="bottom">
+        <p>Copyright  2024 code with vaibhav. All Right Reserved</p>
+        <a href="admin panel/login.php">become a seller</a>
+    </div>
+</footer>
